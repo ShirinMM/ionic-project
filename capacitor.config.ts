a8771691c5e565c59a666a50c7b846d49fde17f6
Shirin_MM <shirinrussel@gmail.com>
2024-07-23 17:06:27 +0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'criosProject',
+  webDir: 'www'
+};
+
+export default config;
